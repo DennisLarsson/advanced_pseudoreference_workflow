f8a884c0d7aab26be7bf7f980ecbac9a3e4461d8
DennisLarsson <dennis.j.larsson@gmail.com>
2024-06-20 14:18:20 +0200

    Remove part of test that checks for files since they won't be found

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 
 nextflow run main.nf --samples_json test_samples.json --popmap popmap_test
-
-while IFS= read -r LINE; do
-    FILE=$(echo "$LINE" | cut -f 1)
-    test -f ${FILE}.fq.gz || { echo "${FILE}.fq.gz not found" && exit 1; }
-done < popmap_test
